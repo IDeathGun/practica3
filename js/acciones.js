e8d7e3eb9d1bf99725558c279823dc18480ea8ed
IDeathGun <JR.ramirez.HQ@gmail.com>
2015-12-01 08:00:25 -0600

    dos

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,19 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+	$('#btn_todas').on('tap', function(){
+      $.ajax({
+		 url: "http://192.168.1.70/practica03/buscartodas.php",
+		 error: function(){
+			alert ("ERROR, no me puedo conectar"); 
+		 },
+		 success: function(respuesta){
+		  alert(respuesta);
+		 }
+		  
+	  });//ajax todas
+
+	});//tap todas
 }); 
 });
 
