806439cc6a76263bfdfea13db95c0b3da4dba8cb
IDeathGun <JR.ramirez.HQ@gmail.com>
2015-12-01 08:09:56 -0600

    cuatro

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$('#btn_todas').on('tap', function(){
       $.ajax({
-		 url: "http://192.168.1.70/practica03/buscartodas.php",
+		 url: "http://192.168.1.170/practica03/buscartodas.php",
 		 error: function(){
 			alert ("ERROR, no me puedo conectar"); 
 		 },
