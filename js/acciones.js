ada8e0090c82f2a1acc144534a3924fa9e6989c3
Lanix 08 <Lanix 08>
2015-12-07 11:19:46 -0600

    ocho
    ya esta la lista de peliculas

--- js/acciones.js
+++ js/acciones.js
@@ -4,18 +4,14 @@ document.addEventListener("deviceready",function(){
 	$('#btnListado').on ('tap',function (){
 		$.ajax({
 			type:"POST",
-<<<<<<< HEAD
 			url:"http://192.168.1.198/practica03/buscartodas.php",
-=======
-			url:"http://192.168.1.35/practica3/buscartodas.php",
->>>>>>> origin/master
 			data:"",
 			error: function(){
 			alert("error en conecxion");},
 			success: function(respuesta){ var producto = JSON.parse(respuesta);
 			$('#contenido').empty();
-			for(var $x=0; $x < producto.peliculas.lenght;$x++)
-			{$('#contenido').append('  ');}
+			for(var $x=0; $x < producto.peliculas.length;$x++)
+			{$('#contenido').append('<div align="center"><div align="left"><img src="http://192.168.1.198/practica03/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula  +'.jpg" /></div><div align="center"><h1>Nombre pelicula: '+ producto.peliculas[$x].nombrePelicula +'</h1></div><div align="center"><h1>Genero: '+  producto.peliculas[$x].generoPelicula +'</h1></div><div align="center"><h1>Clasificacion: '+  producto.peliculas[$x].clasificacionPelicula +'</h1></div><div align="center"><h1>Duracion: '+  producto.peliculas[$x].duracionPelicula +'</h1></div><div align="center"><h1>Idioma: '+  producto.peliculas[$x].idiomaPelicula +'</h1></div><div align="left"><h1>Sipnosis: '+  producto.peliculas[$x].sipnosisPelicula +'</h1></div></div></div>');}
 			$(':mobile-pagecontainer').pagecontainer('change','#todas', { transition: 'pop'});
 			}
 			});
