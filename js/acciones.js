e632f6cd3c41f9ed029feea5289e251d08a56337
Roberto <jr.ramirez.hq@gmail.com>
2015-12-08 09:53:24 -0600

    doce
    ya listo

--- js/acciones.js
+++ js/acciones.js
@@ -1,22 +1,63 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	$('#btnListado').on ('tap',function (){
-		$.ajax({
-			type:"POST",
-			url:"http://192.168.1.198/practica03/buscartodas.php",
-			data:"",
-			error: function(){
-			alert("error en conecxion");},
-			success: function(respuesta){ var producto = JSON.parse(respuesta);
-			$('#contenido').empty();
-			for(var $x=0; $x < producto.peliculas.length;$x++)
-			{$('#contenido').append('<div align="center"><div align="left"><img src="http://192.168.1.198/practica03/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula  +'.jpg" /></div><div align="center"><h1>Nombre pelicula: '+ producto.peliculas[$x].nombrePelicula +'</h1></div><div align="center"><h1>Genero: '+  producto.peliculas[$x].generoPelicula +'</h1></div><div align="center"><h1>Clasificacion: '+  producto.peliculas[$x].clasificacionPelicula +'</h1></div><div align="center"><h1>Duracion: '+  producto.peliculas[$x].duracionPelicula +'</h1></div><div align="center"><h1>Idioma: '+  producto.peliculas[$x].idiomaPelicula +'</h1></div><div align="left"><h1>Sipnosis: '+  producto.peliculas[$x].sipnosisPelicula +'</h1></div></div></div>');}
-			$(':mobile-pagecontainer').pagecontainer('change','#todas', { transition: 'pop'});
-			}
-			});
-			});
-			
-}); 
+	$("#btnTodo").on('tap', function(){
+		 $.ajax({
+			  type: "POST",
+			  url: "http://192.168.1.198/practica03/buscartodas.php",
+			  data: "",
+			  error: function(){
+				  alert("No se puede conectar al servidor");
+				  },
+		      success: function(respuesta)
+			  {
+				  var producto =JSON.parse(respuesta);
+				  
+			   $('#contenido').empty();
+			   
+			   
+			   
+			   for(var $x=0; $x<producto.peliculas.length;$x++)
+			   
+			   {
+				   
+				   $('#contenido').append('<div><div>Nombre de la Película:'+ producto.peliculas[$x].nombrePelicula +'</div><div><div style="float:left; width:30%;"><img src="http://192.168.1.198/practica03/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula +'.jpg"></div><div style="float:left; width:70%;"><div><div style="float:left; width:50%;">Clasificación:'+ producto.peliculas[$x].clasificacionPelicula +'</div><div style="float:left; width:50%;">Género:'+ producto.peliculas[$x].genero +'</div></div><div style="clear:both;"><div style="float:left; width:50%;">Idioma:'+ producto.peliculas[$x].idiomaPelicula +'</div><div style="float:left; width:50%;">Duración:'+ producto.peliculas[$x].duracionPelicula +'</div></div></div></div><div style="clear:both;">Sinópsis:'+ producto.peliculas[$x].sinopsis +'</div></div>');
+				   }
+			   $(':mobile-pagecontainer').pagecontainer('change','#todas',{
+				transition:'pop'
+				});
+				}
+		 });
+		 });
+					 
+		  $("#btnbuscar").on('tap', function(){
+		 $.ajax({
+			  type: "POST",
+			  url: "http://192.168.1.127/proyecto/BusquedaNombre.php",
+			  data: "nombrePeli=" + $('#txtnombrePelicula').val(),
+			  error: function(){
+				  alert("No se puede conectar al servidor");
+				  },
+		      success: function(respuestanombre)
+			  {
+				  var productonombre =JSON.parse(respuestanombre);
+				  
+			   $('#contenidoNombre').empty();
+			   
+			   
+			   
+			   for(var $x=0; $x<productonombre.peliculas.length;$x++)
+			   
+			   {
+				   
+				   $('#contenidoNombre').append('<div><div>Nombre de la Película:'+ productonombre.peliculas[$x].nombrePelicula +'</div><div><div style="float:left; width:30%;"><img src="http://192.168.1.127/proyecto/recursos/imagenes/fotos/'+ productonombre.peliculas[$x].clavePelicula +'.jpg" style="height:20px;width:20px;"></div><div style="float:left; width:70%;"><div><div style="float:left; width:50%;">Clasificación:'+ productonombre.peliculas[$x].clasificacion +'</div><div style="float:left; width:50%;">Género:'+ productonombre.peliculas[$x].genero +'</div></div><div style="clear:both;"><div style="float:left; width:50%;">Idioma:'+ productonombre.peliculas[$x].idioma +'</div><div style="float:left; width:50%;">Duración:'+ productonombre.peliculas[$x].duracion +'</div></div><div style="clear:both;">Director:'+ productonombre.peliculas[$x].director +'</div></div></div><div style="clear:both;">Sinópsis:'+ productonombre.peliculas[$x].sinopsis +'</div></div>');
+				   }
+			   $(':mobile-pagecontainer').pagecontainer('change','#ResultadoNombre',{
+				transition:'pop'
+				});
+				}
+		 });
+		 });
+        
+}); 										
 });
-
